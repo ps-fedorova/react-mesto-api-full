18bd7b4790bc1c82fcbc5e0e1dd050c507196286
Polina Fedorova <ps.fedorova@yandex.ru>
2020-10-06 15:41:35 +0500

    refactor: make the path dynamic

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,16 +1,19 @@
 const winston = require('winston');
 const expressWinston = require('express-winston');
+const path = require('path');
+
+const dirPathLogs = path.join(__dirname, '../logs');
 
 const requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.File({ filename: 'logs/request.log' }),
+    new winston.transports.File({ filename: path.join(dirPathLogs, 'request.log') }),
   ],
   format: winston.format.json(),
 });
 
 const errorLogger = expressWinston.errorLogger({
   transports: [
-    new winston.transports.File({ filename: 'logs/error.log' }),
+    new winston.transports.File({ filename: path.join(dirPathLogs, 'error.log') }),
   ],
   format: winston.format.json(),
 });
